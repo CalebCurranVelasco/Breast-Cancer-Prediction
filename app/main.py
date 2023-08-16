1a2aa6505b6815f33b2ffa6050c039f584d67901
Caleb Curran-Velasco <calebcurran.velasco@gmail.com>
2023-08-16 17:12:31 -0600

    path updated

--- app/main.py
+++ app/main.py
@@ -184,7 +184,7 @@ def main():
         initial_sidebar_state='expanded'
     )
 
-    with open("../assets/style.css") as f:
+    with open("assets/style.css") as f:
         st.markdown("<style>{}</style>".format(f.read()),unsafe_allow_html=True)
 
     input_data = add_sidebar()
