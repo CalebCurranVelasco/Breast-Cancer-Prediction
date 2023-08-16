60559ad4df10ffada104263728bf6c7001333b60
Caleb Curran-Velasco <calebcurran.velasco@gmail.com>
2023-08-16 17:13:16 -0600

    path updated

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def get_clean_data():
-    data = pd.read_csv("../data/data.csv")
+    data = pd.read_csv("data/data.csv")
     
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
     data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B':0 })
